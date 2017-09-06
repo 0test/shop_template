14f9941d7a6cc571509760cff3ab61a59029a047
1px <0test@mail.ru>
2017-09-06 18:46:30 +0300

    Легкие правки в контактах

--- assets/templates/theme/js/contact.js
+++ assets/templates/theme/js/contact.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
-	var yourStartLatLng = new google.maps.LatLng(51.5112, -0.1198);
+	var yourStartLatLng = new google.maps.LatLng(55.7539787,37.6201435);
 	$('#map_canvas').gmap({'center': yourStartLatLng});
 	$('#map_canvas').gmap('option', 'zoom', 14);
-	$('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '51.5112, -0.1198', 'bounds': false } ).click(function() {
-                $('#map_canvas').gmap('openInfoWindow', { 'content': 'Your Office Location' }, this);
+	$('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '55.7539787,37.6201435', 'bounds': false } ).click(function() {
+                $('#map_canvas').gmap('openInfoWindow', { 'content': 'Мы тут' }, this);
         });
 	
 });
